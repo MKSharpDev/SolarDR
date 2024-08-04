5cdafda66559ffdf834b48411a09fc65404b1bc3
M K <kuzz.ma@yandex.ru>
2024-08-04 19:12:43 +0300

    message distribution service

---
+++ SolarDR.MailSender/APIWorker.cs
@@ -0,0 +1,41 @@
+﻿
+using Newtonsoft.Json;
+
+namespace SolarDR.MailSender
+{
+    public class APIWorker
+    {
+        private readonly string _apiAdress;
+        public APIWorker(string apiAdress) 
+        {
+            _apiAdress = apiAdress;
+        }
+
+
+        public async Task<ICollection<PersonForEmail>> GetPersonsAsync()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(_apiAdress);
+
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var personResponse = JsonConvert.DeserializeObject<ICollection<PersonForEmail>>(responseContent);
+                 
+                    return personResponse;
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ SolarDR.MailSender/DRSorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarDR.MailSender
+{
+    public class DRSorter
+    {
+
+        public ICollection<PersonForEmail> SearchTodayBirthday(ICollection<PersonForEmail> persons)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+            var inThisMoth = persons.Where(t => t.Date.Month == today.Month).ToList();
+            var todayBirthday = inThisMoth.Where(t => t.Date.Day == today.Day).ToList();
+
+            return todayBirthday;
+        }
+    }
+}
