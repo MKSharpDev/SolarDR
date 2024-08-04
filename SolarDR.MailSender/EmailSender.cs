1503ae4f1e72654193a757c71f955e2f9859f53c
M K <kuzz.ma@yandex.ru>
2024-08-05 01:13:43 +0300

    fix

--- SolarDR.MailSender/EmailSender.cs
+++ SolarDR.MailSender/EmailSender.cs
@@ -25,30 +25,33 @@ namespace SolarDR.MailSender
             try
             {
                 var persons = await _worker.GetPersonsAsync();
+                
                 var personForEmail = _sorter.SearchTodayBirthday(persons);
-
-                StringBuilder sb = new StringBuilder();
-                sb.AppendLine("Сегодня дР!!");
-
-                foreach (var item in personForEmail)
+                if (personForEmail.Count != 0)
                 {
-                    sb.AppendLine($"{item.Name} {item.Name} {item.Date}");
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("Сегодня дР!!");
+
+                    foreach (var item in personForEmail)
+                    {
+                        sb.AppendLine($"{item.Name} {item.Name} {item.Date}");
+                    }
+                    var body = sb.ToString();
+
+                    Console.WriteLine($"Email was sent at {DateTime.Now}");
+                    string from = "bithd@moikomp.ru";
+                    string to = _options.Email; // Адресс получателя приделах почтового сервера mail.ru
+                    string head = "Hello Test!"; // Тема письма
+
+                    string smtpserver = "mxs.mail.ru";
+                    SmtpClient client = new SmtpClient(smtpserver, 25);
+
+                    client.Send(from, to, head, body);
                 }
-                var body = sb.ToString();
-
-                Console.WriteLine($"Email was sent at {DateTime.Now}");
-                string from = "bithd@moikomp.ru";
-                string to = _options.Email; // Адресс получателя приделах почтового сервера mail.ru
-                string head = "Hello Test!"; // Тема письма
-
-                string smtpserver = "mxs.mail.ru";
-                SmtpClient client = new SmtpClient(smtpserver, 25);
-
-                client.Send(from, to, head, body);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
             }
         }
     }
